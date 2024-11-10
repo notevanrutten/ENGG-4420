7686f5969acff8e791833024515264f7edcf6272
Evan Rutten <contact@evanrutten.com>
2024-11-10 01:24:31 -0500

    Remove stdbool

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -23,7 +23,6 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "stdbool.h"
 #include "usbd_cdc_if.h"
 /* USER CODE END Includes */
 
